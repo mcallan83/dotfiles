fb12b39475ec8477901219cd4930ddc2ae7744a4
Mike Callan <mcallan83@gmail.com>
2013-08-29 22:56:33 -0500

    added slate config

---
+++ install.sh
@@ -0,0 +1,5 @@
+# Clone dotfiles from GitHub into ~/.dotfiles
+#git clone https://github.com/mcallan83/dotfiles.git ~/.dotfiles
+
+# Create symbolic links to dotfiles in ~/.dotfiles
+ln -s ~/.dotfiles/.slate ~/.slate
