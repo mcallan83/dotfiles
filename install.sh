456765335314988b3c985ec306d01347792799dd
Mike Callan <mcallan83@gmail.com>
2013-09-04 00:36:49 -0500

    moved all bash stuff to bash folder

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #link dotfiles to home dir
-ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -s ~/.dotfiles/.slate ~/.slate
