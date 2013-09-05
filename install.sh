3ab1ff69e5ba8e5b342014e91326d2541584b03f
Mike Callan <mcallan83@gmail.com>
2013-09-04 22:08:44 -0500

    added vim stuff to install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -s ~/.dotfiles/app_settings/slate/.slate ~/.slate
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim ~/.vim
