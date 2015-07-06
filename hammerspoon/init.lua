b7b11d2849dbfe90a03e01cfdf7622775ef9fb22
Mike Callan <mcallan83@gmail.com>
2015-07-06 18:10:26 -0500

    initial hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,28 @@
+-- init grid
+hs.grid.MARGINX     = 0
+hs.grid.MARGINY     = 0
+hs.grid.GRIDWIDTH   = 2
+hs.grid.GRIDHEIGHT  = 2
+
+-- disable animation
+hs.window.animationDuration = 0
+
+-- keyboard modifiers
+local mash = {"ctrl", "alt"}
+local mash_app = {"cmd", "ctrl", "alt"}
+local super = {"cmd", "ctrl", "alt", "shift"}
+
+-- reload config
+hs.hotkey.bind(super, "R", function() hs.reload() end)
+hs.notify.new({ title='Hammerspoon', informativeText='Config loaded' }):send():release()
+
+-- resize windows
+hs.hotkey.bind(mash , 'H', hs.grid.pushWindowLeft)
+hs.hotkey.bind(mash, 'L', hs.grid.pushWindowRight)
+
+-- toggle window hints
+hs.hotkey.bind(mash, '.', hs.hints.windowHints)
+
+-- adjust grid size @todo
+-- hs.hotkey.bind(mash, '=', function() hs.grid.adjustWidth( 1) end)
+-- hs.hotkey.bind(mash, '-', function() hs.grid.adjustWidth(-1) end)
