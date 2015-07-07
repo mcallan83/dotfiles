eb752979a5a5f310f677250a831bcd9174ff4793
Mike Callan <mcallan83@gmail.com>
2015-07-07 18:48:13 -0500

    draft brew stuff

--- osx/brew/brew.sh
+++ osx/brew/brew.sh
@@ -1,28 +1,135 @@
-tap 'caskroom/cask'
-tap 'homebrew/bundle'
-tap 'homebrew/dupes'
-tap 'homebrew/php'
+# tap 'caskroom/cask'
+# tap 'homebrew/bundle'
+# tap 'homebrew/dupes'
+# tap 'homebrew/php'
 
-brew 'cask'
-brew 'caskroom/cask/brew-cask'
-brew 'coreutils'
-brew 'git'
-brew 'homebrew/php/composer'
-brew 'homebrew/php/php56'
-brew 'node'
-brew 'tmux'
+# brew 'cask'
+# brew 'caskroom/cask/brew-cask'
+# brew 'coreutils'
+# brew 'git'
+# brew 'homebrew/php/composer'
+# brew 'homebrew/php/php56'
+# brew 'node'
+# brew 'tmux'
 
-cask 'adium'
-cask 'cakebrew'
-cask 'cyberduck'
-cask 'firefox'
-cask 'google-drive'
-cask 'hammerspoon'
-cask 'handbrake'
-cask 'keyboardcleantool'
-cask 'launchcontrol'
-cask 'launchrocket'
-cask 'libreoffice'
-cask 'slate'
-cask 'vagrant'
-cask 'virtualbox'
+# cask 'adium'
+# cask 'cakebrew'
+# cask 'cyberduck'
+# cask 'firefox'
+# cask 'google-drive'
+# cask 'hammerspoon'
+# cask 'handbrake'
+# cask 'keyboardcleantool'
+# cask 'launchcontrol'
+# cask 'launchrocket'
+# cask 'libreoffice'
+# cask 'slate'
+# cask 'vagrant'
+# cask 'virtualbox'
+
+# caskroom/cask
+# homebrew/dupes
+# homebrew/php
+# homebrew/versions
+# thoughtbot/formulae
+
+# caskroom/cask/brew-cask
+# coreutils
+# ctags
+# fig
+# git
+# thoughtbot/formulae/gitsh
+# homebrew/dupes/grep
+# htop-osx
+# imagemagick
+# jq
+# kdiff3
+# node
+# phantomjs
+# homebrew/php/php56-intl
+# homebrew/php/php56-mcrypt
+# homebrew/php/phpunit
+# profanity
+# homebrew/dupes/rsync
+# the_silver_searcher
+# tig
+# tmux
+# wget
+# boot2docker
+# brew-cask
+# composer
+# coreutils
+# ctags
+# docker
+# fig
+# fortune
+# freetype
+# fzf
+# gcc46
+# gdbm
+# gettext
+# git
+# gitsh
+# glib
+# gmp
+# gmp4
+# gnutls
+# grep
+# htop-osx
+# icu4c
+# imagemagick
+# jpeg
+# jq
+# kdiff3
+# libevent
+# libffi
+# libgcrypt
+# libgpg-error
+# libksba
+# libmpc08
+# libotr
+# libpng
+# libstrophe
+# libtasn1
+# libtiff
+# libtool
+# libyaml
+# mcrypt
+# mhash
+# mpfr2
+# nettle
+# node
+# openssl
+# pcre
+# phantomjs
+# php-code-sniffer
+# php-cs-fixer
+# php56
+# php56-intl
+# php56-mcrypt
+# phpmd
+# phpunit
+# pkg-config
+# ppl011
+# profanity
+# python
+# qt
+# readline
+# rsync
+# sqlite
+# the_silver_searcher
+# tig
+# tmux
+# unixodbc
+# wget
+# xz
+# zlib
+# cakebrew
+# gitbook
+# hammerspoon
+# jewelrybox
+# numi
+# sourcetree
+# vagrant
+# vagrant-manager
+# virtualbox
