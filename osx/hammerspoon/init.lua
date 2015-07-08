0b91dfcd9e8afdc770d3a2e2cf329ba8796542c3
Mike Callan <mcallan83@gmail.com>
2015-07-07 21:32:56 -0500

    auto load hammerspoon config

--- osx/hammerspoon/init.lua
+++ osx/hammerspoon/init.lua
@@ -1,12 +1,6 @@
 -- https://github.com/cmsj/hammerspoon-config/blob/master/init.lua
 -- https://github.com/BrianGilbert/.hammerspoon/blob/master/init.lua
 
--- init grid
-hs.grid.MARGINX     = 0
-hs.grid.MARGINY     = 0
-hs.grid.GRIDWIDTH   = 2
-hs.grid.GRIDHEIGHT  = 2
-
 -- keyboard modifiers
 local mash = {"cmd", "shift"}
 local mash_app = {"cmd", "ctrl", "alt"}
@@ -15,7 +9,7 @@ local hyper = {"cmd", "ctrl", "alt", "shift"}
 -- disable window animations
 hs.window.animationDuration = 0
 
--- size window to left half
+-- move window to left half
 hs.hotkey.bind(mash, "H", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -28,7 +22,7 @@ hs.hotkey.bind(mash, "H", function()
   win:setFrame(f)
 end)
 
--- size window to right half
+-- move window to right half
 hs.hotkey.bind(mash, "L", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -52,3 +46,18 @@ hs.hotkey.bind(mash_app, 'C', function() hs.application.launchOrFocus('Google Ch
 hs.hotkey.bind(mash_app, 'I', function() hs.application.launchOrFocus('iTerm') end)
 hs.hotkey.bind(mash_app, 'S', function() hs.application.launchOrFocus('Sublime Text') end)
 hs.hotkey.bind(mash_app, 'M', function() hs.application.launchOrFocus('Sequel Pro') end)
+
+-- auto reload config
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.dotfiles/osx/hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
