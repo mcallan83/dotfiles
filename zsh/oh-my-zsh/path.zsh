088ed3582ffd3d862a4f314ec16c4862f5c271ab
Mike Callan <mcallan83@gmail.com>
2015-07-11 18:29:10 -0500

    updated path for homebrew php

--- zsh/oh-my-zsh/path.zsh
+++ zsh/oh-my-zsh/path.zsh
@@ -5,16 +5,24 @@ if [ -x /usr/libexec/path_helper ]; then
     eval `/usr/libexec/path_helper -s`
 fi
 
+# rvm
 if [ -d $HOME/.rvm/bin ] ; then
-    PATH="$HOME/.rvm/bin"
+    PATH="$HOME/.rvm/bin:$PATH"
 fi
 
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
+# personal bin folder
+if [ -d "$HOME/.bin" ] ; then
+    PATH="$HOME/.bin:$PATH"
+fi
+
+# homebrew php
+if [ -d "/usr/local/opt/php56/bin" ] ; then
+    PATH="/usr/local/opt/php56/bin:$PATH"
 fi
 
 PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
+# composer
 if [ -d "$HOME/.composer/vendor/bin" ] ; then
     PATH="$PATH:$HOME/.composer/vendor/bin"
 fi
