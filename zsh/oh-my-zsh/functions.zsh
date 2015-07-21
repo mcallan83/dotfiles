1467eddfaa463418d1f6fa9258b5d19ae40b555b
Mike Callan <mcallan83@gmail.com>
2015-07-20 19:24:50 -0500

    cleanup

--- zsh/oh-my-zsh/functions.zsh
+++ zsh/oh-my-zsh/functions.zsh
@@ -1,21 +1,18 @@
 # mkdir and cd to it
-function mkcd
-{
+function mkcd {
     dir="$*";
     mkdir -p "$dir" && cd "$dir";
 }
 
 # displays terminal color palette
 # http://askubuntu.com/questions/27314/script-to-display-all-terminal-colors
-function colortest
-{
+function colortest {
     for x in 0 1 4 5 7 8; do for i in `seq 30 37`; do for a in `seq 40 47`; do echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m\e[0;37;40m "; done; echo; done; done; echo "";
 }
 
 # run adminer as web server if installed
 if [ -d /usr/local/share/adminer ] ; then
-    function adminer
-    {
+    function adminer {
             open http://localhost:8888
             php -S localhost:8888 -t /usr/local/share/adminer > /dev/null
     }
