8fbd14eb0f820035ac157a384c34c70fd6bc958f
Mike Callan <mcallan83@gmail.com>
2015-09-27 22:03:17 -0500

    osx install cleanup

--- osx/install.sh
+++ osx/install.sh
@@ -1,6 +1,8 @@
 sudo -v
 
-# brew taps
+################################################################################
+# additional homebrew repos
+################################################################################
 brew tap caskroom/cask
 brew tap caskroom/versions
 brew tap homebrew/bundle
@@ -11,7 +13,9 @@ brew tap homebrew/services
 brew tap homebrew/versions
 brew tap thoughtbot/formulae
 
+################################################################################
 # install brews
+################################################################################
 brew install bash-completion
 brew install boot2docker
 brew install caskroom/cask/brew-cask
@@ -38,7 +42,7 @@ brew install vim --override-system-vi
 brew install wget --with-iri
 brew install zsh
 
-# install php
+# install php56 and add settings
 brew install homebrew/php/php56
 echo "date.timezone = America/Chicago" >> /usr/local/etc/php/5.6/php.ini
 echo "phar.readonly = Off" >> /usr/local/etc/php/5.6/php.ini
@@ -51,8 +55,11 @@ brew install homebrew/php/php56-mcrypt
 brew install homebrew/php/phploc
 brew install homebrew/php/phpmd
 
+################################################################################
 # install brew casks
+################################################################################
 sudo brew cask
+
 brew cask install adium
 brew cask install alfred
 brew cask install appcleaner
@@ -66,7 +73,7 @@ brew cask install firefox
 brew cask install font-open-sans
 brew cask install font-source-code-pro
 brew cask install forklift
-brew cask install google-chrome # move app to /Applications for 1Password
+brew cask install google-chrome
 brew cask install google-drive
 brew cask install google-hangouts
 brew cask install hammerspoon
@@ -89,7 +96,13 @@ brew cask install virtualbox
 brew cask install vlc
 brew cask install xquartz
 
+ # move google chrome to ~/Applications for 1Password
+rm -rf ~/Applications/Google\ Chrome.app
+ mv /opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app/ ~/Applications/Google\ Chrome.app/
+
+################################################################################
 # brew cleanup
+################################################################################
 brew cleanup
 brew upgrade
 brew cleanup
@@ -98,13 +111,18 @@ brew outdated
 brew prune
 brew cask cleanup
 
+################################################################################
 # install composer globals
+################################################################################
 composer global require "jakub-onderka/php-parallel-lint=0.*"
 composer global require "laravel/homestead=~2.0"
 composer global require "laravel/installer=~1.1"
 composer global require "phpunit/phpunit=4.8.*"
 
+
+################################################################################
 # install npm globals
+################################################################################
 npm install -g browser-sync
 npm install -g grunt-cli
 npm install -g gulp
