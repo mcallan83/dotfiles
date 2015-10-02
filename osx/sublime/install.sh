2a64121d8e9abd51218602cb7b780719be100fdd
Mike Callan <mcallan83@gmail.com>
2015-10-02 14:05:48 -0500

    sublime install script

---
+++ osx/sublime/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+CONFIG="$HOME/Library/Application Support/Sublime Text 3/Packages"
+
+# create config directory
+mkdir -p "$CONFIG"
+
+# backup existing settings
+if [ -d "$CONFIG/User" ]; then
+    echo "Sublime: Backing Up Settings"
+    mv "$CONFIG/User" "$CONFIG/User.$(date +%F-%T).bak"
+fi
+
+# symlink settings
+echo "Sublime: Symlinking Settings"
+cd "$CONFIG"
+ln -s "$HOME/.dotfiles/osx/sublime" User
+
+echo "Sublime: Done"
