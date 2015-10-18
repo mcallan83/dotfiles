60d5995b8d4b72352e13336c50e56bb6e918cb14
Mike Callan <mcallan83@gmail.com>
2015-10-18 12:53:07 -0500

    hammerspoon cleanup

--- osx/hammerspoon/config/init.lua
+++ osx/hammerspoon/config/init.lua
@@ -5,44 +5,15 @@
 ---------------------------------------------1-----------------------------------
 
 -- keyboard modifiers
-local mash = {"cmd", "shift"}
-local mash_app = {"cmd", "ctrl", "alt"}
 local hyper = {"cmd", "ctrl", "alt", "shift"}
 
 -- disable window animations
 hs.window.animationDuration = 0
 
--- move window to left half
-hs.hotkey.bind(mash, "H", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
--- move window to right half
-hs.hotkey.bind(mash, "L", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
--- maximize window
-hs.hotkey.bind(mash, 'RETURN', hs.grid.maximizeWindow)
-
--- toggle window hints
-hs.hotkey.bind(mash, '.', hs.hints.windowHints)
+-- resize windows left, right, and full
+hs.hotkey.bind(hyper, '[', function() hs.window.focusedWindow():moveToUnit(hs.layout.left50) end)
+hs.hotkey.bind(hyper, ']', function() hs.window.focusedWindow():moveToUnit(hs.layout.right50) end)
+hs.hotkey.bind(hyper, 'SPACE', hs.grid.maximizeWindow)
 
 -- toggle apps
 hs.hotkey.bind(hyper, 'C', function() toggle_application('Google Chrome') end)
