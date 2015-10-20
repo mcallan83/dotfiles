d08ddf553a8e3058ad79c6954ab92f82da6f58d1
Mike Callan <mcallan83@gmail.com>
2015-10-19 23:56:43 -0500

    wait for rvm file to be installed

--- osx/01_install.sh
+++ osx/01_install.sh
@@ -204,6 +204,10 @@ npm install -g yo
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --autolibs=homebrew
 
+while : ; do
+    [[ -f "$HOME/.rvm/scripts/rvm" ]] && break
+    sleep 1
+done
 source "$HOME/.rvm/scripts/rvm"
 
 rvm install 2.1.1
