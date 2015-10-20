6ba744ef9dccc655a1e32204fd1ad32c571598be
Mike Callan <mcallan83@gmail.com>
2015-10-19 22:50:46 -0500

    vagrant test

--- osx/01_install.sh
+++ osx/01_install.sh
@@ -146,11 +146,11 @@ brew cask install font-source-code-pro
 
 # @todo -- test
 # allow vagrant to mount NFS without password
-# sudo tee "/etc/sudoers" > /dev/null <<EOF
-# Cmnd_Alias VAGRANT_EXPORTS_ADD = /usr/bin/tee -a /etc/exports
-# Cmnd_Alias VAGRANT_NFSD = /sbin/nfsd restart
-# Cmnd_Alias VAGRANT_EXPORTS_REMOVE = /usr/bin/sed -E -e /*/ d -ibak /etc/exports
-# %admin ALL=(root) NOPASSWD: VAGRANT_EXPORTS_ADD, VAGRANT_NFSD, VAGRANT_EXPORTS_REMOVE
+sudo tee "/etc/sudoers" > /dev/null <<EOF
+Cmnd_Alias VAGRANT_EXPORTS_ADD = /usr/bin/tee -a /etc/exports
+Cmnd_Alias VAGRANT_NFSD = /sbin/nfsd restart
+Cmnd_Alias VAGRANT_EXPORTS_REMOVE = /usr/bin/sed -E -e /*/ d -ibak /etc/exports
+%admin ALL=(root) NOPASSWD: VAGRANT_EXPORTS_ADD, VAGRANT_NFSD, VAGRANT_EXPORTS_REMOVE
 EOF
 
 
