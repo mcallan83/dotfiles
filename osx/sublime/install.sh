9a10de643bacb7be8ce04859ef1038489427e0b2
Mike Callan <mcallan83@gmail.com>
2015-10-26 21:13:01 -0500

    install package controll for sublime

--- osx/sublime/install.sh
+++ osx/sublime/install.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
-CONFIG="$HOME/Library/Application Support/Sublime Text 3/Packages"
+CONFIG="$HOME/Library/Application Support/Sublime Text 3"
 
-# create config directory
-mkdir -p "$CONFIG"
+# create directories
+mkdir -p "$CONFIG/Packages"
+mkdir -p "$CONFIG/Installed Packages"
+
+# install package manager
+if [ ! -f "$CONFIG/Installed Packages/Package Control.sublime-package" ]; then
+    echo "Sublime: Installing Package Manager"
+    cd "$CONFIG/Installed Packages"
+    curl -O "https://packagecontrol.io/Package Control.sublime-package" > /dev/null
+    cd - > /dev/null
+fi
 
 # backup existing settings
-if [ -d "$CONFIG/User" ] || [ -f "$CONFIG/User" ]; then
+if [ -d "$CONFIG/Packages/User" ] || [ -f "$CONFIG/Packages/User" ]; then
     echo "Sublime: Backing Up Settings"
-    mv "$CONFIG/User" "$CONFIG/User.$(date +%s).bak"
+    mv "$CONFIG/Packages/User" "$CONFIG/Packages/User.$(date +%s).bak"
 fi
 
 # symlink settings
 echo "Sublime: Symlinking Settings"
-cd "$CONFIG"
+cd "$CONFIG/Packages"
 ln -s "$HOME/.dotfiles/osx/sublime/config" User
 
 echo "Sublime: Done"
