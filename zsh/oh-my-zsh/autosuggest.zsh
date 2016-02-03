9c4f29ee9e92e400f9520406fca8be44c3d6e9b6
Mike Callan <mcallan83@gmail.com>
2016-02-02 19:14:43 -0600

    zsh autocomplete

---
+++ zsh/oh-my-zsh/autosuggest.zsh
@@ -0,0 +1,15 @@
+################################################################################
+# Filename: zsh/oh-my-zsh/autosuggest.zsh
+# Author: Mike Callan
+# URL: http://github.com/mcallan83/dotfiles
+#
+# Enables ZSH autosuggestions.
+################################################################################
+
+zle-line-init() {
+    zle autosuggest-start
+}
+zle -N zle-line-init
+
+bindkey '^f' vi-forward-word
+bindkey '^x' vi-end-of-line
