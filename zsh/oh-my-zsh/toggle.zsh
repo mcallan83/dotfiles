afcdc392ce96a82c0a5b424c417f20e8da5e2d2d
Mike Callan <mcallan83@gmail.com>
2016-02-22 18:57:23 -0600

    zsh toggle between fg app and cli

---
+++ zsh/oh-my-zsh/toggle.zsh
@@ -0,0 +1,14 @@
+# use ctrl+z to toggle between fg app and commandline
+
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
