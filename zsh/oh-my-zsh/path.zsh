ec9f5bfe4184b4084c94234a0050ea6c4badb1a8
Mike <mcallan83@gmail.com>
2019-10-25 15:53:16 -0500

    Clean up $PATH

--- zsh/oh-my-zsh/path.zsh
+++ zsh/oh-my-zsh/path.zsh
@@ -9,19 +9,9 @@ PATH="$PATH:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"
 # remove duplicates
 typeset -U PATH
 
-# php
-if [ -d "/usr/local/opt/php@$DOTFILES_PHP_VERSION" ] ; then
-    PATH="/usr/local/opt/php@$DOTFILES_PHP_VERSION/bin:/usr/local/opt/php@$DOTFILES_PHP_VERSION/sbin:$PATH"
-fi
-
 # dotfiles bin folder
 if [ -d "$DOTFILES/zsh/oh-my-zsh/bin" ] ; then
     PATH="$PATH:$DOTFILES/zsh/oh-my-zsh/bin"
-fi
-
-# composer
-if [ -d "$HOME/.composer/vendor/bin" ] ; then
-    PATH="$PATH:$HOME/.composer/vendor/bin"
 fi
 
 # rvm - complains if not first
