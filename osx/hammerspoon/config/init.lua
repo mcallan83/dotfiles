532a691e7c0efb7fa0ccc3a74a24dea24fcb7d8f
Mike <mcallan83@gmail.com>
2020-08-20 11:36:16 -0500

    add code to connect to airpods in hammerspoon

--- osx/hammerspoon/config/init.lua
+++ osx/hammerspoon/config/init.lua
@@ -123,3 +123,43 @@ for i, binding in ipairs(windowBindings) do
 end
 
 hs.notify.new({title='Hammerspoon', informativeText='Config Loaded'}):send()
+
+-- connect to airpods
+function airPods(deviceName)
+  local s = [[
+    activate application "SystemUIServer"
+    tell application "System Events"
+      tell process "SystemUIServer"
+        set btMenu to (menu bar item 1 of menu bar 1 whose description contains "bluetooth")
+        tell btMenu
+          click
+  ]]
+  ..
+          'tell (menu item "' .. deviceName .. '" of menu 1)\n'
+  ..
+  [[
+            click
+            if exists menu item "Connect" of menu 1 then
+              click menu item "Connect" of menu 1
+              return "Connecting AirPods..."
+            else
+              click menu item "Disconnect" of menu 1
+              return "Disconecting AirPods..."
+            end if
+          end tell
+        end tell
+      end tell
+    end tell
+  ]]
+
+  return hs.osascript.applescript(s)
+end
+
+hs.hotkey.bind(hyper, "\\", function()
+  local ok, output = airPods('Mike’s Airpods Pro')
+  if ok then
+    hs.alert.show(output)
+  else
+    hs.alert.show("Couldn't connect to AirPods!")
+  end
+end)
