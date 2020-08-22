a5fb9e30f2dab440f09d4838742c183b8ed41321
Mike <mcallan83@gmail.com>
2020-08-22 13:31:59 -0500

    Refactor install.sh to use Brewfile

--- osx/install.sh
+++ osx/install.sh
@@ -43,117 +43,65 @@ if test ! $(which brew); then
 fi
 
 brew analytics off
+brew bundle install --no-lock --file=$(dirname "$0")/Brewfile
+brew bundle cleanup -f
+brew doctor
+brew cask doctor
 
-banner "Tapping Additional Homebrew Repos"
-brew tap homebrew/cask-fonts
+################################################################################
+# Fixing Permissions
+################################################################################
 
-banner "Installing Homebrew Packages"
-brew install coreutils
-brew install ctop
-brew install ddrescue
-brew install fd
-brew install ffmpeg
-brew install fzf
-brew install git
-brew install github/gh/gh
-brew install go
-brew install gpg
-brew install htop-osx
-brew install hudochenkov/sshpass/sshpass
-brew install jq
-brew install nmap
-brew install rclone
-brew install reattach-to-user-namespace
-brew install shellcheck
-brew install ssh-copy-id
-brew install starship
-brew install testdisk
-brew install the_silver_searcher
-brew install tmux
-brew install tree
-brew install vim
-brew install wget
-brew install xmlstarlet
-brew install youtube-dl
-brew install zsh
+banner "Fixing Permissions"
 
-banner "Fixing ZSH File Permissions"
 chmod go-w /usr/local/share/zsh
 chmod go-w /usr/local/share/zsh/site-functions
 
-# Homebrew Casks
-banner "Installing Homebrew Casks"
-brew cask install alfred
-brew cask install angry-ip-scanner
-brew cask install arq
-brew cask install balenaetcher
-brew cask install barrier
-brew cask install betterzip
-brew cask install ccleaner
-brew cask install cyberduck
-brew cask install disk-inventory-x
-brew cask install docker
-brew cask install google-backup-and-sync
-brew cask install google-chrome
-brew cask install hammerspoon
-brew cask install handbrake
-brew cask install iterm2
-brew cask install kaleidoscope
-brew cask install karabiner-elements
-brew cask install keepassxc
-brew cask install keyboardcleantool
-brew cask install launchcontrol
-brew cask install libreoffice
-brew cask install numi
-brew cask install sequel-pro
-brew cask install slack
-brew cask install spotify
-brew cask install sublime-text
-brew cask install suspicious-package
-brew cask install the-unarchiver
-brew cask install transmission
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install visual-studio-code
-brew cask install vlc
-
-brew cask install qlcolorcode
-brew cask install qlmarkdown
-brew cask install qlprettypatch
-brew cask install qlstephen
-brew cask install qlvideo
-brew cask install quicklook-csv
-brew cask install quicklook-json
-brew cask install webpquicklook
-
-brew cask install font-fira-code
-brew cask install font-source-code-pro
-
 ################################################################################
-# PHP\Composer
+# PHP
 ################################################################################
 
-banner "Installing PHP"
+banner "Configuring PHP"
 
-brew install php@7.3
 echo "date.timezone = America/Chicago" >> /usr/local/etc/php/7.3/php.ini
 echo "phar.readonly = Off" >> /usr/local/etc/php/7.3/php.ini
 echo "pcre.jit = 0" >> /usr/local/etc/php/7.3/php.ini
 
-brew install php@7.4
 echo "date.timezone = America/Chicago" >> /usr/local/etc/php/7.4/php.ini
 echo "phar.readonly = Off" >> /usr/local/etc/php/7.4/php.ini
 
 brew list | grep php | xargs -L1 brew unlink
 brew link --force --overwrite php@7.4
 
-brew install composer
-
 composer global require consolidation/cgr
 
 cgr hirak/prestissimo
 cgr laravel/installer
 cgr laravel/lumen-installer
+
+composer self-update
+composer global update
+
+################################################################################
+# Node Version Manager
+################################################################################
+
+banner "Install Node Version Manager"
+
+mkdir "$HOME/.nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install 12
+npm install -g vmd @vue/cli yarn
+npm update -g
+nvm install 10 --reinstall-packages-from=12
+npm update -g
+
+nvm alias default 12
+nvm use default
 
 ################################################################################
 # Vagrant
@@ -195,41 +143,3 @@ if [ $? -eq 0 ]; then
 fi
 
 rm -f $TMP
-
-################################################################################
-# Node Version Manager
-################################################################################
-
-mkdir "$HOME/.nvm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-nvm install 12
-npm install -g vmd @vue/cli yarn
-nvm install 10 --reinstall-packages-from=12
-
-nvm alias default 12
-nvm use default
-
-################################################################################
-# Update and Cleanup
-################################################################################
-
-banner "Update and Cleanup"
-
-# homebrew
-brew update
-brew upgrade
-brew cleanup
-brew doctor
-brew cask doctor
-
-# composer
-composer self-update
-composer global update
-
-# npm/yarn
-npm install -g npm
-npm update -g
