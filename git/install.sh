9a1f151a7ee0a51cbb35301c8dca90d118ddde00
Mike <mcallan83@gmail.com>
2020-08-28 13:59:53 -0500

    Add diff-so-fancy

--- git/install.sh
+++ git/install.sh
@@ -34,6 +34,24 @@ if [[ $(uname) == 'Darwin' ]]; then
     git config --global credential.helper osxkeychain
 fi
 
+# https://github.com/so-fancy/diff-so-fancy
+if [ -x "$(command -v diff-so-fancy)" ]
+then
+    git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+    git config --global color.ui true
+    git config --global color.diff-highlight.oldNormal    "red bold"
+    git config --global color.diff-highlight.oldHighlight "red bold 52"
+    git config --global color.diff-highlight.newNormal    "green bold"
+    git config --global color.diff-highlight.newHighlight "green bold 22"
+    git config --global color.diff.meta       "11"
+    git config --global color.diff.frag       "magenta bold"
+    git config --global color.diff.commit     "yellow bold"
+    git config --global color.diff.old        "red bold"
+    git config --global color.diff.new        "green bold"
+    git config --global color.diff.whitespace "red reverse"
+    git config --bool --global diff-so-fancy.markEmptyLines false
+fi
+
 # append git aliases
 git config --global alias.up "!git pull --rebase --prune $@ && git submodule update --init --recursive"
 git config --global alias.sub "!git submodule foreach git pull origin master"
