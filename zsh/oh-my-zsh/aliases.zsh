99325c40fe27237dc6304e22aacd29ba928605e6
Mike <mcallan83@gmail.com>
2020-08-30 20:04:03 -0500

    Update aliases

--- zsh/oh-my-zsh/aliases.zsh
+++ zsh/oh-my-zsh/aliases.zsh
@@ -6,9 +6,14 @@
 ################################################################################
 
 
-# edit config files in editor
+# open config files in editor
 alias hosts="sudo $EDITOR /etc/hosts"
-alias sshconfig="$EDITOR ~/.ssh/config"
+alias sshc="$EDITOR ~/.ssh/config"
+alias sshk="$EDITOR ~/.ssh/known_hosts"
+
+# global aliases
+alias -g G='| grep'
+alias -g L='| less'
 
 # ansible
 alias ap="ansible-playbook"
@@ -18,14 +23,11 @@ alias ave="ansible-vault encrypt"
 # cd back
 alias cdb="cd -"
 
-# colortest
-alias colortest="colortest | sed -n '1!p'"
-
 # composer update with no memory limits
 alias cr="composer require"
 alias cu="composer update"
 
-# docker
+# docker compose
 alias dcu="docker-compose up"
 alias dcub="docker-compose up --build"
 
@@ -37,18 +39,6 @@ alias gt="cd \$(git rev-parse --show-toplevel)"
 # github
 alias ghb="gh repo view --web"
 alias ghpr="gh pr create"
-
-# grep
-alias -g G='| grep'
-
-# json pretty print
-alias -g J='| python -m json.tool'
-
-# edit known_hosts file in editor
-alias known="$EDITOR ~/.ssh/known_hosts"
-
-# less
-alias -g L='| less'
 
 # ps
 alias psa="ps aux"
@@ -62,19 +52,12 @@ alias ss="stt"
 # tail
 alias t="tail -f"
 
-# tmux
-alias tb="tmux show-buffer"
-alias mux="tmuxinator"
-
 # vagrant
 alias vc="vagrant ssh -c"
 alias vh="vagrant halt"
 alias vr="vagrant reload"
 alias vs="vagrant ssh"
 alias vu="vagrant up"
-
-# vim
-alias v="vim"
 
 # yarn
 alias yw="yarn watch"
@@ -84,8 +67,23 @@ alias yw="yarn watch"
 ################################################################################
 
 # ansible 2.8
-alias ansible-playbook='docker run --rm -it -v "$(pwd)":/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --workdir=/ansible willhallonline/ansible:2.8-alpine ansible-playbook'
-alias ansible-vault='docker run --rm -it -v "$(pwd)":/ansible -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --workdir=/ansible willhallonline/ansible:2.8-alpine ansible-vault'
+alias ansible-playbook="docker run \
+    --rm \
+    -it \
+    -v \"$(pwd)\":/ansible \
+    -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+    --workdir=/ansible \
+    willhallonline/ansible:2.8-alpine \
+    ansible-playbook"
+
+alias ansible-vault="docker run \
+    --rm \
+    -it \
+    -v \"$(pwd)\":/ansible \
+    -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+    --workdir=/ansible \
+    willhallonline/ansible:2.8-alpine \
+    ansible-vault"
 
 # docker clean
 alias dccl="docker run --rm -v /var/run/docker.sock:/var/run/docker.sock zzrot/docker-clean"
