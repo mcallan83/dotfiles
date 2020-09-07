469ba3c619ee55420c93b19485f5ef928ffaa73b
Mike Callan <mcallan83@gmail.com>
2020-09-07 09:35:45 -0500

    Move sublime config

---
+++ apps/sublime/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+################################################################################
+# Filename: apps/sublime/install.sh
+# Author: Mike Callan
+# URL: http://github.com/mcallan83/dotfiles
+#
+# Installs Sublime configuration, including Package Control and various
+# packages. Sublime may need to be restarted multiple times befores
+# all packages install correctly.
+################################################################################
+
+
+DESTINATION_PATH="$HOME/Library/Application Support/Sublime Text 3"
+
+# create directories
+mkdir -p "$DESTINATION_PATH/Packages"
+mkdir -p "$DESTINATION_PATH/Installed Packages"
+
+# install package manager
+if [ ! -f "$DESTINATION_PATH/Installed Packages/Package Control.sublime-package" ]; then
+    echo "Sublime: Installing Package Manager"
+    (
+        # shellcheck disable=SC2164
+        cd "$DESTINATION_PATH/Installed Packages"
+        curl -O "https://packagecontrol.io/Package Control.sublime-package" > /dev/null
+    )
+fi
+
+# backup existing settings
+if [ -L "$DESTINATION_PATH/Packages/User" ]; then
+    echo "Sublime: Backing Up Settings"
+    mv "$DESTINATION_PATH/Packages/User" "$DESTINATION_PATH/Packages/User.$(date +%s).bak"
+fi
+
+# symlink settings
+echo "Sublime: Symlinking Settings"
+ln -s "$DOTFILES/apps/sublime/config" "$DESTINATION_PATH/Packages/User"
+echo "Sublime: Done"
