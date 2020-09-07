3213be150d5dcaee8f0dcf032590f3b50dbd703b
Mike Callan <mcallan83@gmail.com>
2020-09-07 11:37:45 -0500

    Remove autosuggest

--- apps/utils.sh
+++ apps/utils.sh
@@ -2,7 +2,7 @@
 
 # ask for confirmation
 function confirm() {
-    read -r -p "$1 [yY]:"
+    read -r -p "$1 [Yy]:"
     case $(echo "$REPLY" | tr '[:upper:]' '[:lower:]') in
         y|yes) ;;
         *)     echo "Skipping." && exit 0 ;;
