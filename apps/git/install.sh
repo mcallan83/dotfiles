9321014b814d7052ccc2474c0a52a3faee58de24
Mike Callan <mcallan83@gmail.com>
2020-09-06 21:30:05 -0500

    Move git config to apps folder

--- apps/git/install.sh
+++ apps/git/install.sh
@@ -1,19 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 ################################################################################
-# Filename: git/install.sh
+# Filename: apps/git/install.sh
 # Author: Mike Callan
 # URL: http://github.com/mcallan83/dotfiles
 #
-# Install GIT configuration.
+# Configure GIT.
 ################################################################################
 
 GITCONFIG="$HOME/.gitconfig"
 
 # ask for name and email address
-read -p "Full Name [Mike Callan]: " NAME
+read -r -p "Full Name [Mike Callan]: " NAME
 NAME=${NAME:-Mike Callan}
 
-read -p "Email [mcallan83@gmail.com]: " EMAIL
+read -r -p "Email [mcallan83@gmail.com]: " EMAIL
 EMAIL=${EMAIL:-mcallan83@gmail.com}
 
 # backup existing settings
@@ -24,8 +25,8 @@ fi
 
 # build .gitconfig
 echo "Git: Installing Git Configuration"
-git config --global user.name $NAME
-git config --global user.email $EMAIL
+git config --global user.name "$NAME"
+git config --global user.email "$EMAIL"
 git config --global push.default simple
 git config --global pull.rebase false
 
@@ -53,5 +54,5 @@ then
 fi
 
 # append git aliases
-git config --global alias.up "!git pull --rebase --prune $@ && git submodule update --init --recursive"
+git config --global alias.up "!git pull --rebase --prune $* && git submodule update --init --recursive"
 git config --global alias.sub "!git submodule foreach git pull origin master"
