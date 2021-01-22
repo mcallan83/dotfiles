52bd6ca231466008e57aab28d9d54c958a5f359b
Mike Callan <mcallan83@gmail.com>
2021-01-22 11:28:11 -0600

    Add list open ports alias

--- zsh/common/aliases.zsh
+++ zsh/common/aliases.zsh
@@ -10,12 +10,18 @@ alias hosts="sudo $EDITOR /etc/hosts"
 alias sshc="$EDITOR ~/.ssh/config"
 alias sshk="$EDITOR ~/.ssh/known_hosts"
 
+# disk space report
+alias diskspace="df -P -kHl"
+
 # global aliases
 alias -g G='| grep'
 alias -g L='| less'
 
 # cd to previous directory
 alias cdp="cd -"
+
+# list open ports
+alias ports='sudo nmap -sT -O localhost'
 
 # ps
 alias psa="ps aux"
