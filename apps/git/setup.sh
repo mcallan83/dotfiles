6a24c272f836c23b82d4177702e37572f6a91a18
Mike Callan <mcallan83@gmail.com>
2022-03-31 22:08:01 -0500

    Add global .gitignore

--- apps/git/setup.sh
+++ apps/git/setup.sh
@@ -1,26 +1,41 @@
 #!/usr/bin/env bash
 
 GITCONFIG="$HOME/.gitconfig"
+GITIGNORE="$HOME/.gitignore"
 
 # ask for name and email address
 read -r -p "Full Name [Mike Callan]: " NAME
 NAME=${NAME:-Mike Callan}
 
-read -r -p "Email [mcallan83@gmail.com]: " EMAIL
-EMAIL=${EMAIL:-mcallan83@gmail.com}
+DEFAULT_EMAIL="$(echo 'zpnyyna83@tznvy.pbz' | tr 'A-Za-z' 'N-ZA-Mn-za-m')"
+read -r -p "Email [$DEFAULT_EMAIL]: " EMAIL
+EMAIL=${EMAIL:-$DEFAULT_EMAIL}
 
-# backup existing settings
+# backup .gitconfig
 if [ -f "$GITCONFIG" ]; then
-    echo "Git: Backing Up Settings"
+    echo "Git: Backing Up Config"
     mv "$GITCONFIG" "$GITCONFIG.$(date +%s).bak"
 fi
+
+# backup .gitignore
+if [ -f "$GITIGNORE" ]; then
+    echo "Git: Backing Up Ignore File"
+    mv "$GITIGNORE" "$GITIGNORE.$(date +%s).bak"
+fi
+
+# create .gitignore
+cat >> "$GITIGNORE" <<EOF
+*.DS_Store
+*.swp
+EOF
 
 # build .gitconfig
 echo "Git: Installing Git Configuration"
 git config --global user.name "$NAME"
 git config --global user.email "$EMAIL"
+git config --global core.excludesfile ~/.gitignore
 git config --global push.default simple
-git config --global pull.rebase false
+#git config --global pull.rebase false
 
 # https://github.com/so-fancy/diff-so-fancy
 if [ -x "$(command -v diff-so-fancy)" ]
