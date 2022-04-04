20a412a36bf6ced14711fbe1f9aab5788fa09ef0
Mike Callan <mcallan83@gmail.com>
2022-04-04 17:38:20 -0500

    rename post includes

--- zsh/common/aliases.zsh
+++ zsh/common/aliases.zsh
@@ -4,8 +4,6 @@ alias -g L='| less'
 
 # open various config files in editor
 alias hosts="sudo $EDITOR /etc/hosts"
-alias sshc="$EDITOR ~/.ssh/config"
-alias sshk="$EDITOR ~/.ssh/known_hosts"
 
 # cd to previous directory
 alias cdp="cd -"
