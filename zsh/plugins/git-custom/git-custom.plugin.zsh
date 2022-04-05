db961b471ae0b5d0cc211776a67b8558c47a26e3
Mike Callan <mcallan83@gmail.com>
2022-04-04 19:54:49 -0500

    cleanup git plugin

--- zsh/plugins/git-custom/git-custom.plugin.zsh
+++ zsh/plugins/git-custom/git-custom.plugin.zsh
@@ -1,9 +1,13 @@
-# aliases
+################################################################################
+# Git Plugin
+#
+# Register custom aliases and functions for git and gh.
+################################################################################
+
 alias gj="git checkout -"
 alias gsub="git submodule update --recursive --remote"
 alias gt="cd \$(git rev-parse --show-toplevel)"
 
-# github
 alias ghb="gh repo view --web"
 alias ghpr="gh pr create"
 
