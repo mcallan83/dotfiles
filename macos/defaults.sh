c418a67d806a6d302700593fedaed9ae219359b8
Mike Callan <mcallan83@gmail.com>
2022-04-04 22:24:48 -0500

    Update macos defaults

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# https://github.com/mathiasbynens/dotfiles/blob/main/.macos
+
+################################################################################
 # General
+################################################################################
+
+# disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
 
 # expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
@@ -10,37 +17,65 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-# save to disk (not to iCloud) by default
+# save to disk (not to icloud) by default
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
 # automatically quit printer app once the print jobs complete
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# enable full keyboard access for all controls
+# disable the “are you sure you want to open this application?” dialog
+# defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+################################################################################
+# Keyboard\Mouse\Trackpad
+################################################################################
+
+# trackpad: enable tap to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# keyboard: enable full access for all controls
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# disable press-and-hold for keys in favor of key repeat
+# keyboard: disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-# set keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 6
-defaults write NSGlobalDomain InitialKeyRepeat -int 25
+# keyboard: set repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+# defaults write NSGlobalDomain KeyRepeat -int 6
+# defaults write NSGlobalDomain InitialKeyRepeat -int 25
 
+################################################################################
 # Finder
+################################################################################
 
-# finder: show all filename extensions
+# disable window animations animations
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# finder: show status bar
+# show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# finder: show path bar
+# show path bar
 defaults write com.apple.finder ShowPathbar -bool true
 
 # disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
+# avoid creating ".DS_Store" files on network or usb volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# use list view by default
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+################################################################################
 # Dock
+################################################################################
 
 # set the icon size of Dock items to 36 pixels
 defaults write com.apple.dock tilesize -int 36
@@ -51,7 +86,12 @@ defaults write com.apple.dock autohide -bool true
 # don’t show recent applications in Dock
 defaults write com.apple.dock show-recents -bool false
 
+# wipe all app icons from the Dock
+# defaults write com.apple.dock persistent-apps -array
+
+################################################################################
 # Safari
+################################################################################
 
 # show the full URL in the address bar
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
@@ -59,37 +99,63 @@ defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 # enable debug menu
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
-# enable the Develop menu and the Web Inspector in Safari
+# enable the develop menu and the web inspector in Safari
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-
-# add a context menu item for showing the Web Inspector in web views
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
+# update extensions automatically
+defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
+
+################################################################################
 # Activity Monitor
+################################################################################
 
-# Show the main window when launching Activity Monitor
+# show the main window when launching
 defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 
-# Visualize CPU usage in the Activity Monitor Dock icon
+# visualize cpu usage in the dock icon
 defaults write com.apple.ActivityMonitor IconType -int 5
 
-# Show all processes in Activity Monitor
+# show all processes
 defaults write com.apple.ActivityMonitor ShowCategory -int 0
 
-# Sort Activity Monitor results by CPU usage
+# Sort results by cpu usage
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
 
-# Chrome
+################################################################################
+# App Store
+################################################################################
 
-# Disable the all too sensitive backswipe on trackpads
+# enable automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+# check for software updates daily
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+# turn on app auto-update
+defaults write com.apple.commerce AutoUpdate -bool true
+
+# Allow rebooting machine on macOS updates
+defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
+
+################################################################################
+# Chrome
+################################################################################
+
+# disable backswipe on trackpads
 defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
 defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
 
-# Disable the all too sensitive backswipe on Magic Mouse
-defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
-defaults write com.google.Chrome.canary AppleEnableMouseSwipeNavigateWithScrolls -bool false
+################################################################################
+# Visual Studio Code
+################################################################################
+
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
 echo "Done. Some changes need a restart to take effect."
