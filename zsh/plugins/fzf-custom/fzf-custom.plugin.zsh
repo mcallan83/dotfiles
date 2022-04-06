4f28c3ca4a06ede35d99865dbc45eba6e754e773
Mike Callan <mcallan83@gmail.com>
2022-04-06 02:13:20 -0500

    Add chrome bookmark search to FZF and cleanup

--- zsh/plugins/fzf-custom/fzf-custom.plugin.zsh
+++ zsh/plugins/fzf-custom/fzf-custom.plugin.zsh
@@ -1,19 +1,34 @@
-# Important: This plugin must be loaded after the "z" plugin.
+# This plugin must be loaded after the "z" plugin.
+
+# open a chrome bookmark
+# https://betterprogramming.pub/meet-fzf-a-fuzzy-finder-to-enhance-your-command-line-workflow-a2890f6a70f8
+bb() {
+     bookmarks_path=~/Library/Application\ Support/Google/Chrome/Default/Bookmarks
+
+     jq_script='
+        def ancestors: while(. | length >= 2; del(.[-1,-2]));
+        . as $in | paths(.url?) as $key | $in | getpath($key) | {name,url, path: [$key[0:-2] | ancestors as $a | $in | getpath($a) | .name?] | reverse | join("/") } | .path + "/" + .name + "\t" + .url'
+
+    jq -r "$jq_script" < "$bookmarks_path" \
+        | sed -E $'s/(.*)\t(.*)/\\1\t\x1b[36m\\2\x1b[m/g' \
+        | fzf --ansi \
+        | cut -d$'\t' -f2 \
+        | xargs open
+}
 
 # checkout local or remote git branch
 # https://github.com/wellle/dotfiles/blob/577d9ee28f772b2f57e44c00a3d392c3238660eb/fzf.zsh#L110
-fbr() {
+br() {
   git fetch
   local branches branch
   branches=$(git branch --all | grep -v HEAD | grep -v dependabot) &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  branch=$(echo "$branches" | fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
 
 # search chrome history
 # https://sourabhbajaj.com/mac-setup/iTerm/fzf.html
-fch() {
+hh() {
   local cols sep
   cols=$(( COLUMNS / 3 ))
   sep='{::}'
@@ -25,29 +40,6 @@ fch() {
      from urls order by last_visit_time desc" |
   awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
   fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs open
-}
-
-# list and connect to configured ssh servers (bound to ^s)
-# https://gist.github.com/dohq/1dc702cc0b46eb62884515ea52330d60
-function fssh () {
-  local selected_host=$(grep "Host " ~/.ssh/config | grep -v '*' | cut -b 6- | sort -r | fzf --query "$LBUFFER")
-
-  if [ -n "$selected_host" ]; then
-    BUFFER="ssh ${selected_host}"
-    zle accept-line
-  fi
-  zle reset-prompt
-}
-
-zle -N fssh
-bindkey '^s' fssh
-
-# integrate with z
-# https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/z
-unalias z 2> /dev/null
-z() {
-  [ $# -gt 0 ] && _z "$*" && return
-  cd "$(_z -l 2>&1 | fzf --height 40% --reverse --inline-info +s --tac --query "$*" | sed 's/^[0-9,.]* *//')"
 }
 
 # paste selected file path into current command (bound to ^t)
@@ -83,3 +75,25 @@ zle     -N            fzf-file-widget
 bindkey -M emacs '^T' fzf-file-widget
 bindkey -M vicmd '^T' fzf-file-widget
 bindkey -M viins '^T' fzf-file-widget
+
+# list and connect to configured ssh servers (bound to ^s)
+# https://gist.github.com/dohq/1dc702cc0b46eb62884515ea52330d60
+function fzf-ssh-widget() {
+  local selected_host=$(grep "Host " ~/.ssh/config | grep -v '*' | cut -b 6- | sort -r | fzf --query "$LBUFFER")
+  if [ -n "$selected_host" ]; then
+    BUFFER="ssh ${selected_host}"
+    zle accept-line
+  fi
+  zle reset-prompt
+}
+
+zle -N fzf-ssh-widget
+bindkey '^s' fzf-ssh-widget
+
+# integrate with z
+# https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/z
+unalias z 2> /dev/null
+z() {
+  [ $# -gt 0 ] && _z "$*" && return
+  cd "$(_z -l 2>&1 | fzf --height 40% --reverse --inline-info +s --tac --query "$*" | sed 's/^[0-9,.]* *//')"
+}
