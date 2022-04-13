5cbfbe3a53d8c61795b5389b09fb61a5da448d6e
Mike Callan <mcallan83@gmail.com>
2022-04-13 00:06:00 -0500

    add gh aliases

--- zsh/plugins/git-custom/git-custom.plugin.zsh
+++ zsh/plugins/git-custom/git-custom.plugin.zsh
@@ -3,6 +3,8 @@ alias gsub="git submodule update --recursive --remote"
 alias gt="cd \$(git rev-parse --show-toplevel)"
 
 alias ghb="gh repo view --web"
+alias ghi="gh issue list --search \"sort:created-desc\""
+alias ghim="gh issue list --assignee \"@me\""
 alias ghpr="gh pr create"
 
 # init a git repo
