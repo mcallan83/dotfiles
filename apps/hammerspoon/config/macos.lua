1db456d687e2cf28f902331f144721c92ff46cb2
Mike Callan <mcallan83@gmail.com>
2023-02-12 12:52:16 -0600

    Update clear notificatins script

--- apps/hammerspoon/config/macos.lua
+++ apps/hammerspoon/config/macos.lua
@@ -3,15 +3,17 @@ local actions = {
   clearNotifications = function()
     hs.applescript([[
       tell application "System Events"
-          tell process "Notification Center"
-              set theWindows to every window
-              repeat with i from 1 to number of items in theWindows
-                  set this_item to item i of theWindows
-                  try
-                      click button 1 of this_item
-                  end try
-              end repeat
-          end tell
+        try
+          set _groups to groups of UI element 1 of scroll area 1 of group 1 of window "Notification Center" of application process "NotificationCenter"
+          repeat with _group in _groups
+            set _actions to actions of _group
+            repeat with _action in _actions
+              if description of _action is in {"Close", "Clear All"} then
+                perform _action
+              end if
+            end repeat
+          end repeat
+        end try
       end tell
     ]])
   end
