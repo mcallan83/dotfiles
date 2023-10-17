ebf0baf1f3635c807506d3abf85ee33e2d89883e
Mike Callan <mcallan83@gmail.com>
2023-10-17 08:48:07 -0500

    add php related ag search functions for zsh

--- zsh/plugins/php-homebrew-custom/php-homebrew-custom.plugin.zsh
+++ zsh/plugins/php-homebrew-custom/php-homebrew-custom.plugin.zsh
@@ -3,3 +3,13 @@ function phpv {
     brew list --formula | grep php | xargs -L1 brew unlink
     brew link --force --overwrite "php@$1"
 }
+
+# ag search in vendor folder
+function vag {
+    ag $1 vendor
+}
+
+# ag search in vendor/laravel folder
+function lag {
+    ag $1 vendor/laravel
+}
