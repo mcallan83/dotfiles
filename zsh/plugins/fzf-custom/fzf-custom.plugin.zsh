03af2e232f924203a2d0870d59b2fa47ef63d8bb
Mike Callan <mcallan83@gmail.com>
2024-03-27 13:49:15 -0500

    Fix FZF config

--- zsh/plugins/fzf-custom/fzf-custom.plugin.zsh
+++ zsh/plugins/fzf-custom/fzf-custom.plugin.zsh
@@ -1,7 +1,6 @@
 # This plugin must be loaded after the "z" plugin.
 
-source "/usr/local/opt/fzf/shell/completion.zsh"
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+eval "$(fzf --zsh)"
 
 # checkout local or remote git branch
 # https://github.com/wellle/dotfiles/blob/577d9ee28f772b2f57e44c00a3d392c3238660eb/fzf.zsh#L110
