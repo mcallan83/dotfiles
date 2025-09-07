a5e86ddafbfe2d00438ad25d53ac9f011c1d4d46
Mike Callan <mcallan83@gmail.com>
2025-09-07 12:02:46 -0500

    Add method to diff file renames with GIT

--- zsh/plugins/git-custom/git-custom.plugin.zsh
+++ zsh/plugins/git-custom/git-custom.plugin.zsh
@@ -7,3 +7,12 @@ alias ghi="gh issue list --search \"sort:created-desc\""
 alias ghis="gh issue status"
 alias ghiv="gh issue view"
 alias ghpr="gh pr create"
+
+git-rename-diff() {
+  git diff -M -z --name-status --diff-filter=R HEAD | \
+  while IFS= read -r -d '' _ && IFS= read -r -d '' old && IFS= read -r -d '' new; do
+    git --no-pager diff --no-index --color-words=. --unified=0 \
+      <(echo "$old") <(echo "$new") | tail -n +5 | tr -d '\n'
+    echo
+  done
+}
